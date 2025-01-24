3581fc7b311650f9d0a934038837d155ed9c81f8
Joseph Stone <stonepreston05@gmail.com>
2025-01-23 22:04:32 -0700

    changed the home page back to original

--- galaxy-trade/app/page.tsx
+++ galaxy-trade/app/page.tsx
@@ -1,9 +1,6 @@
 // Home page which will diplay ItemCard.tsx component
 
-import { FC } from 'react';
-
-const Home: FC = () => {
-  items();
+export default function Home() {
   return (
     <div>
       <h1>Welcome to GalaxyTrade 🌌</h1>
@@ -11,25 +8,3 @@ const Home: FC = () => {
     </div>
   );
 };
-
-export default Home;
-
-export async function items() {
-  try {
-    const response = await fetch('http://localhost:3000/api/items/itemRoutes', {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-
-    const data = await response.json();
-    console.log('items', data);
-  } catch (error) {
-    console.error('Error fetching items:', error);
-  }
-}
